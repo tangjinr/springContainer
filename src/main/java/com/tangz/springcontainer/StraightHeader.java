bd9c0e9dab566358d5d653eb01cc7913b5d33968
tangjinr <tangpersonal@163.com>
2017-04-27 13:29:28 +0800

    2017-04-27 application-contest.xml通过setter方法依赖注入

--- src/main/java/com/tangz/springcontainer/StraightHeader.java
+++ src/main/java/com/tangz/springcontainer/StraightHeader.java
@@ -1,26 +1,31 @@
 package com.tangz.springcontainer;
 
-import java.util.Map;
-import java.util.Properties;
-
 public class StraightHeader implements Header {
 
     private String color;
     private int size;
 
-    public StraightHeader(String color, int size) {
+//    public StraightHeader(String color, int size) {
+//        this.color = color;
+//        this.size = size;
+//    }
+//
+//    public StraightHeader(Map<String, String> paras) {
+//        this.color = paras.get("color");
+//        this.size = Integer.valueOf(paras.get("size"));
+//    }
+//
+//    public StraightHeader(Properties props) {
+//        this.color = String.valueOf(props.get("color"));
+//        this.size = Integer.valueOf(String.valueOf(props.get("size")));
+//    }
+//
+    public void setColor(String color) {
         this.color = color;
+    }
+
+    public void setSize(int size) {
         this.size = size;
-    }
-
-    public StraightHeader(Map<String, String> paras) {
-        this.color = paras.get("color");
-        this.size = Integer.valueOf(paras.get("size"));
-    }
-
-    public StraightHeader(Properties props) {
-        this.color = String.valueOf(props.get("color"));
-        this.size = Integer.valueOf(String.valueOf(props.get("size")));
     }
 
     @Override
